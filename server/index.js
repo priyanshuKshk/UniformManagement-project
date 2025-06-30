5a0a3c0909d7c9ee37e2b8b5725f700578f8f4c1
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-07-01 00:33:19 +0530

    cors edit

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: "http://localhost:5173"||"https://uniformmanagementproject.onrender.com ",
     credentials: true,
   })
 );
