2e82768920d701220052ecb95089a9aa8ab8c46b
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-07-11 12:06:58 +0530

    cors origin and size changed

--- server/index.js
+++ server/index.js
@@ -12,12 +12,18 @@ const allowedOrigins = [
   "https://uniformmanagementproject.onrender.com"
 ];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 dotenv.config();
--- server/models/Uniform.js
+++ server/models/Uniform.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const UniformSchema = new mongoose.Schema({
   itemName: String,
-  size: String,
+  size: String.toUpperCase(),
   costPrice: Number,
   quantity: Number,
   available: {
-  type: Number,
-  default: 0,
-},
-allotted: {
-  type: Number,
-  default: 0,
-}
-,
-  lastUpdated: { type: Date, default: Date.now }
+    type: Number,
+    default: 0,
+  },
+  allotted: {
+    type: Number,
+    default: 0,
+  },
+  lastUpdated: { type: Date, default: Date.now },
 });
-module.exports = mongoose.model('Uniform', UniformSchema);
+module.exports = mongoose.model("Uniform", UniformSchema);
