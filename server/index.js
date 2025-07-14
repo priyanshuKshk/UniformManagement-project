1dccc93deefcc09de34219aa4408cdc3eac4e275
priyanshuKshk <kshkpriyanshu64@gmail.com>
2025-07-14 21:27:25 +0530

    dotenv file

--- server/index.js
+++ server/index.js
@@ -7,14 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-
+dotenv.config();
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:3000",
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
 }));
 
 app.use(express.json());
-dotenv.config();
+
 // MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI)
